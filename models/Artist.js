78ba343adb995c9120c18e3638942194c19e95c0
Mykiel-Horn <mykiel.d.horn24@gmail.com>
2021-03-27 18:41:28 -0500

    seed data good

--- models/Artist.js
+++ models/Artist.js
@@ -1,10 +1,34 @@
 const mongoose = require('../db/connection');
-const User = require('./User');
 
 const ArtistSchema = new mongoose.Schema({
-	name: String,
+	artist: String,
+	bio: String,
+	album: [
+		{
+			name: String,
+			yearReleased: String,
+			albumsSold: String,
+			certification: String,
+			stream: String,
+		},
+	],
 	photo: String,
 	likes: [],
+	mixTape: [
+		{
+			albumName: String,
+			yearReleased: String,
+		},
+	],
+	recordLabel: String,
+	genre: String,
+	yearsActive: String,
+	awards: [
+		{
+			grammys: Number,
+			billBoardMusicAwards: String,
+		},
+	],
 });
 
 const Artist = mongoose.model('Artist', ArtistSchema);

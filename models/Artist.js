b966bcaa15fe437822b5e1e8cec4d1ecee8b574f
Patrick <zhen.patrick@gmail.com>
2021-03-27 16:36:41 -0400

    added Artist Schema

--- models/Artist.js
+++ models/Artist.js
@@ -1,6 +1,32 @@
 const mongoose = require("../db/connection");
 
 const artistSchema = new mongoose.Schema({
+  Artist: String,
+  Bio: String,
+  Album: [
+    {
+      Name: String,
+      yearReleased: String,
+      albumsSold: String,
+      Certification: String,
+      stream: String,
+    },
+  ],
+  mixTape: [
+    {
+      albumName: String,
+      yearReleased: String,
+    },
+  ],
+  recordLabel: String,
+  genre: String,
+  yearsActive: String,
+  Awards: [
+    {
+      Grammys: Number,
+      billBoardMusicAwards: String,
+    },
+  ],
 });
 
 const Artist = mongoose.model("Artist", artistSchema);

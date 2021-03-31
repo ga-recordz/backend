75ec4c916e11250cc4914c888868998bb244729e
msenat7432 <maxsenat7432@gmail.com>
2021-03-31 13:05:06 -0400

    changeseed

--- controllers/artists.js
+++ controllers/artists.js
@@ -11,44 +11,44 @@ router.get('/', (req, res, next) => {
 		.catch(next);
 });
 
-// router.get("/:id", (req, res, next) => {
-//   Artist.findById({ _id: req.params.id })
-//     .then((artists) => {
-//       res.json(artists);
-//     })
-//     .catch(next);
-// });
+router.get("/:id", (req, res, next) => {
+  Artist.findById({ _id: req.params.id })
+    .then((artists) => {
+      res.json(artists);
+    })
+    .catch(next);
+});
 
-// router.post("/", (req, res, next) => {
-//   Artist.create(req.body).then(() => {
-//     Artist
-//       .find({})
-//       .then((artists) => {
-//         res.json(artists);
-//       })
-//       .catch(next);
-//   });
-// });
+router.post("/", (req, res, next) => {
+  Artist.create(req.body).then(() => {
+    Artist
+      .find({})
+      .then((artists) => {
+        res.json(artists);
+      })
+      .catch(next);
+  });
+});
 
-// router.delete("/:id", (req, res, next) => {
-//   Artist.findByIdAndDelete({ _id: req.params.id }).then(() => {
-//     Artist.find({}).then((artists) => {
-//       res.json(artists);
-//     });
-//   });
-// });
+router.delete("/:id", (req, res, next) => {
+  Artist.findByIdAndDelete({ _id: req.params.id }).then(() => {
+    Artist.find({}).then((artists) => {
+      res.json(artists);
+    });
+  });
+});
 
-// router.put("/:id", (req, res, next) => {
-//   Artist
-//     .findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
-//     .then(() => {
-//       Artist
-//         .find({})
-//         .then((artists) => {
-//           res.json(artists);
-//         })
-//         .catch(next);
-//     });
-// });
+router.put("/:id", (req, res, next) => {
+  Artist
+    .findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then(() => {
+      Artist
+        .find({})
+        .then((artists) => {
+          res.json(artists);
+        })
+        .catch(next);
+    });
+});
 
 module.exports = router;

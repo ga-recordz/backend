792792f92661e2d5324f67cfd63c5ef409701aa0
Patrick <zhen.patrick@gmail.com>
2021-04-01 12:12:45 -0400

    Added new data

--- models/Artist.js
+++ models/Artist.js
@@ -1,38 +1,46 @@
-const mongoose = require('../db/connection');
+const mongoose = require("../db/connection");
 
 const ArtistSchema = new mongoose.Schema({
-	artist: String,
-	bio: String,
-	likes: [String],
-	debates: [String],
-	album: [
-		{
-			name: String,
-			yearReleased: String,
-			albumsSold: String,
-			certification: String,
-			stream: String,
-		},
-	],
-	photo: String,
+  artist: String,
+  bio: String,
+  likes: [String],
+  debates: [String],
+  image: String,
+  album: [
+    {
+      albumName: String,
+      yearReleased: String,
+      albumSold: String,
+      certification: String,
+      stream: String,
+    },
+  ],
+  photo: String,
 
-	mixTape: [
-		{
-			albumName: String,
-			yearReleased: String,
-		},
-	],
-	recordLabel: String,
-	genre: String,
-	yearsActive: String,
-	awards: [
-		{
-			grammys: Number,
-			billBoardMusicAwards: String,
-		},
-	],
+  mixTapes: [
+    {
+      albumName: String,
+      yearReleased: String,
+    },
+  ],
+  recordLabel: String,
+  genre: String,
+  yearsActive: String,
+  awards: [
+    {
+      grammy: Number,
+      billBoardMusicAwards: Number,
+      betAwards: Number,
+      americanMusicAwards: Number,
+      RockandRollHallofFame: String,
+      BETHipHopAwards: Number,
+      MtvvideoMusicawards: Number,
+      academyAward: Number,
+      MTVawards: Number,
+    },
+  ],
 });
 
-const Artist = mongoose.model('Artist', ArtistSchema);
+const Artist = mongoose.model("Artist", ArtistSchema);
 
 module.exports = Artist;

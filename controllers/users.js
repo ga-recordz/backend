6b479469a75fcca22bb30232f72a243b6c90f14d
Patrick <zhen.patrick@gmail.com>
2021-04-04 18:32:30 -0400

    Added google Ouath

--- controllers/users.js
+++ controllers/users.js
@@ -8,27 +8,57 @@ const router = express.Router();
 // POST /signup
 //Using promise chain
 //get the password -> hash it -> store the hashed password in the database
-router.post('/signup', (req, res, next) => {
-	bcrypt
-		.hash(req.body.password, 10)
-		// return a new object with the email and hashed password
-		.then((hash) =>
-			// when returning an object with fat arrow syntax, we
-			// need to wrap the object in parentheses so JS doesn't
-			// read the object curly braces as the function block
-			({
-				email: req.body.email,
-				password: hash,
-				userName: req.body.userName,
-			})
-		)
-		// create user with provided email and hashed password
-		.then((user) => User.create(user))
-		// send the new user object back with status 201, but `hashedPassword`
-		// won't be sent because of the `transform` in the User model
-		.then((user) => res.status(201).json(user))
-		// pass any errors along to the error handler
-		.catch(next);
+router.post("/signup", (req, res, next) => {
+	if(req.body.thirdPartyId){
+		bcrypt
+      .hash(req.body.password, 10)
+      // return a new object with the email and hashed password
+      .then((hash) =>
+        // when returning an object with fat arrow syntax, we
+        // need to wrap the object in parentheses so JS doesn't
+        // read the object curly braces as the function block
+        ({
+          email: req.body.email,
+          password: hash,
+          userName: req.body.userName,
+		  thirdPartyId: req.body.thirdPartyId
+        })
+      )
+      // create user with provided email and hashed password
+      .then((user) => User.create(user))
+      // send the new user object back with status 201, but hashedPassword
+      // won't be sent because of the transform in the User model
+      .then((user) => res.status(201).json(user))
+      // pass any errors along to the error handler
+      .catch(next);
+	} else {
+		 bcrypt
+    .hash(req.body.password, 10)
+    // return a new object with the email and hashed password
+    .then((hash) =>
+      // when returning an object with fat arrow syntax, we
+      // need to wrap the object in parentheses so JS doesn't
+      // read the object curly braces as the function block
+      ({
+        email: req.body.email,
+        password: hash,
+        userName: req.body.userName,
+      })
+    )
+    // create user with provided email and hashed password
+    .then((user) => User.create(user))
+    // send the new user object back with status 201, but hashedPassword
+    // won't be sent because of the transform in the User model
+    .then((user) => res.status(201).json(user))
+    // pass any errors along to the error handler
+    .catch(next);
+	}
+
+
+
+
+
+ 
 });
 
 // ------------------------------------SIGN IN---------------------------------------
@@ -45,7 +75,7 @@ router.post('/signin', (req, res, next) => {
 		// a token that we'll in turn send to the client.
 		.then((tokenData) => {
 			return res.json(tokenData);
-		})
+		}) 
 		.catch(next);
 });
 
@@ -53,9 +83,18 @@ router.post('/signin', (req, res, next) => {
 router.get('/user/:userID', (req, res, next) => {
 	User.findById(req.params.userID)
 		.then((user) => {
-			res.json(user);
+			if(User){
+				res.json(user)	
+			} else {
+				res.json({isUser: false})
+			}
 		})
-		.catch(next);
 });
 
+//Get user by thirdPartyId
+router.get("/thirdPartyUser/:thirdPartyId", (req, res, next) => {
+  User.findOne({"thirdPartyId": req.params.thirdPartyId}).then((user) => {
+	  return res.json(user)
+  }).catch(next)
+})
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -2,36 +2,39 @@ const mongoose = require('../db/connection');
 const Artist = require('../models/Artist');
 
 const UserSchema = new mongoose.Schema(
-	{
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		userName: {
-			type: String,
-			required: true,
-		},
-		// top5: [Artist],
-		// goat: Artist,
-		// favoriteArtists: [Artist],
-	},
-	{
-		timestamps: true,
-		//Created a virtual that will automatically remove the password field any time a toJSON method like JSON.stringify(), Mongoose's .toJSON() method or Express' .json() method is used
-		toJSON: {
-			virtuals: true,
-			// ret is the returned Mongoose document
-			transform: (_doc, ret) => {
-				delete ret.password;
-				return ret;
-			},
-		},
-	}
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    thirdPartyId: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    // top5: [Artist],
+    // goat: Artist,
+    // favoriteArtists: [Artist],
+  },
+  {
+    timestamps: true,
+    //Created a virtual that will automatically remove the password field any time a toJSON method like JSON.stringify(), Mongoose's .toJSON() method or Express' .json() method is used
+    toJSON: {
+      virtuals: true,
+      // ret is the returned Mongoose document
+      transform: (_doc, ret) => {
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
 );
 
 const User = mongoose.model('User', UserSchema);
